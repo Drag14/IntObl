75440bd420c68ce5de2e04218b3f5b087161865e
Bartosz Minch <bartosz.minch@outlook.com>
2016-11-07 21:41:25 +0100

    Generating some basic 'trail' and make tourist to follow it. TODO: More problematic trails and option of turning back of a tourist.

--- TOPR/agents.py
+++ TOPR/agents.py
@@ -7,7 +7,7 @@ import random
 class Tourist(Agent):
     """Represents a single Tourist cell in the simulation."""
 
-    def __init__(self, pos, model, speed=5, moore=True):
+    def __init__(self, pos, model, speed=5, moore=True, trail_indicator=0):
         """
         grid: The MultiGrid object in which the agent lives.
         x: The agent's current x coordinate
@@ -18,7 +18,8 @@ class Tourist(Agent):
         super().__init__(pos, model)
         self.speed = speed
         self.moore = moore
-
+        self.trail_indicator = trail_indicator
+        self.trail_indicator_prev = trail_indicator
     # @property
     # def neighbors(self):
     #     return self.model.grid.neighbor_iter((self.x, self.y), True)
@@ -27,12 +28,19 @@ class Tourist(Agent):
         """
         Step one cell in any allowable direction.
         """
+
         next_moves = self.model.grid.get_neighborhood(self.pos, self.moore)
         for cell in next_moves:
-            temp = self.model.grid.get_cell_list_contents([cell])
-            if self.model.grid.get_cell_list_contents([cell]):
-                self.model.grid.move_agent(self, cell)
+            # temp = self.model.grid.get_cell_list_contents([cell])
+            x, y = cell
 
+            temp = self.model.grid[x][y]
+            if temp:
+                self.trail_indicator = list(temp)[0].id
+
+            if self.trail_indicator > self.trail_indicator_prev:
+                self.model.grid.move_agent(self, cell)
+                self.trail_indicator_prev = self.trail_indicator
 
 class Trail(Agent):
     def __init__(self, pos, model, unique_id):
